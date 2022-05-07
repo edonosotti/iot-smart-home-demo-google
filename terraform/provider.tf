bec9368dfa3a9c742c0f610ecb06054a8d1b0216
Edoardo Nosotti <edoardo.nosotti@gmail.com>
2022-05-07 12:17:41 +0200

    Remove unused Terraform provider.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,12 +7,3 @@ provider "google" {
   # Note: https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference#user_project_override
   user_project_override = true
 }
-
-#provider "google-beta" {
-#  project               = var.gcp_project
-#  region                = var.gcp_region
-#  zone                  = var.gcp_zone
-#
-#  # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference#user_project_override
-#  user_project_override = true
-#}
